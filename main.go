48a84232867430fc8e7da7b5cf523401f8defa13
Thomas DoyleEngler <tpdoyle87@gmail.com>
2019-08-11 20:32:08 -0500

    creating go app

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	http.ListenAndServe(":3030", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
