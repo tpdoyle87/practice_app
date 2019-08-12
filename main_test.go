baae0767f5e4b390fef767bc9de5008637da832d
Thomas DoyleEngler <tpdoyle87@gmail.com>
2019-08-12 09:02:00 -0500

    finsihed basic tutorial

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -28,5 +29,83 @@ func TestHandler(t *testing.T) {
 	actual := recorder.Body.String()
 	if actual != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", actual, expected)
+	}
+}
+
+func TestRouter(t *testing.T) {
+	r := newRouter()
+
+	mockServer := httptest.NewServer(r)
+
+	resp, err := http.Get(mockServer.URL + "/hello")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Status should be ok, got %d", resp.StatusCode)
+	}
+
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	respString := string(b)
+	expected := "Hello World!"
+
+	if respString != expected {
+		t.Errorf("Response should b %s, got %s", expected, respString)
+	}
+}
+
+func TestRouterForNonExistentRoute(t *testing.T) {
+	r := newRouter()
+	mockServer := httptest.NewServer(r)
+
+	resp, err := http.Post(mockServer.URL+"/hello", "", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf("Status should be 405, got %d", resp.StatusCode)
+	}
+
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	respString := string(b)
+	expected := ""
+
+	if respString != expected {
+		t.Errorf("Response should be %s, got %s", expected, respString)
+	}
+}
+
+func TestStaticFileServer(t *testing.T) {
+	r := newRouter()
+	mockServer := httptest.NewServer(r)
+
+	resp, err := http.Get(mockServer.URL + "/assets/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Status should be 200, got %d", resp.StatusCode)
+	}
+
+	contentType := resp.Header.Get("Content-Type")
+	expectedContentType := "text/html"; "charset=utf-8"
+
+	if expectedContentType != contentType {
+		t.Errorf("Wrong content type, expected %s, got %s", expectedContentType, contentType)
 	}
 }
